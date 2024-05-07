f68e8939cc27d7ef1f67f2b9577322d5e4f37922
David Reis <eudavidreis.dev@gmail.com>
2024-05-06 21:10:41 -0300

    Update - Adicionando parametros as chamadas de endpoint.

--- server.py
+++ server.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Página principal."
-
-@app.route('/ola')
-def ola_mundo():
-    return "Olá, mundo."
-
+ 
+@app.route('/ola/<nome>')
+def ola_mundo(nome):
+    return "Olá, " + nome
+ 
 if __name__ == '__main__':
     app.run()
